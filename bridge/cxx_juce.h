8ce91163d1e3849be6958542178a9359d3e0ef6e
James Hallowell <5460955+JamesHallowell@users.noreply.github.com>
2023-02-02 01:03:11 +0000

    Add IIR filter

--- bridge/cxx_juce.h
+++ bridge/cxx_juce.h
@@ -313,4 +313,71 @@ namespace system_audio_volume
         return juce::SystemAudioVolume::getGain();
     }
 } // namespace system_audio_volume
+
+namespace iir_filter
+{
+    std::unique_ptr<juce::SingleThreadedIIRFilter> createIIRFilter (std::array<rust::f32, 5> coefficients)
+    {
+        auto filter = std::make_unique<juce::SingleThreadedIIRFilter>();
+
+        juce::IIRCoefficients coeffs;
+        coeffs.coefficients[0] = coefficients[0];
+        coeffs.coefficients[1] = coefficients[1];
+        coeffs.coefficients[2] = coefficients[2];
+        coeffs.coefficients[3] = coefficients[3];
+        coeffs.coefficients[4] = coefficients[4];
+
+        filter->setCoefficients (coeffs);
+
+        return filter;
+    }
+
+    std::array<rust::f32, 5> makeLowPass (double sampleRate,
+                                          double cutoffFrequency,
+                                          double q)
+    {
+        const auto coefficients = juce::IIRCoefficients::makeLowPass (
+            sampleRate,
+            cutoffFrequency,
+            q);
+
+        return { coefficients.coefficients[0],
+                 coefficients.coefficients[1],
+                 coefficients.coefficients[2],
+                 coefficients.coefficients[3],
+                 coefficients.coefficients[4] };
+    }
+
+    std::array<rust::f32, 5> makeHighPass (double sampleRate,
+                                           double cutoffFrequency,
+                                           double q)
+    {
+        const auto coefficients = juce::IIRCoefficients::makeHighPass (
+            sampleRate,
+            cutoffFrequency,
+            q);
+
+        return { coefficients.coefficients[0],
+                 coefficients.coefficients[1],
+                 coefficients.coefficients[2],
+                 coefficients.coefficients[3],
+                 coefficients.coefficients[4] };
+    }
+
+    std::array<rust::f32, 5> makeNotchFilter (double sampleRate,
+                                              double cutoffFrequency,
+                                              double q)
+    {
+        const auto coefficients = juce::IIRCoefficients::makeNotchFilter (
+            sampleRate,
+            cutoffFrequency,
+            q);
+
+        return { coefficients.coefficients[0],
+                 coefficients.coefficients[1],
+                 coefficients.coefficients[2],
+                 coefficients.coefficients[3],
+                 coefficients.coefficients[4] };
+    }
+} // namespace iir_filter
 } // namespace cxx_juce
---
+++ src/juce_audio_basics.rs
@@ -0,0 +1,54 @@
+//! Classes for audio buffer manipulation, midi message handling, synthesis, etc.
+
+use {crate::juce, cxx::UniquePtr};
+
+/// An infinite impulse response (IIR) filter.
+pub struct IIRFilter(UniquePtr<juce::SingleThreadedIIRFilter>);
+
+/// The quality (Q) factor of a filter.
+#[derive(Debug, Copy, Clone)]
+pub struct Q(pub f64);
+
+impl Default for Q {
+    fn default() -> Self {
+        Self(1.0 / 2_f64.sqrt())
+    }
+}
+
+impl IIRFilter {
+    /// Create a low-pass filter.
+    pub fn low_pass(sample_rate: f64, frequency: f64, Q(q): Q) -> Self {
+        Self(juce::create_iir_filter(juce::make_low_pass(
+            sample_rate,
+            frequency,
+            q,
+        )))
+    }
+
+    /// Create a high-pass filter.
+    pub fn high_pass(sample_rate: f64, frequency: f64, Q(q): Q) -> Self {
+        Self(juce::create_iir_filter(juce::make_high_pass(
+            sample_rate,
+            frequency,
+            q,
+        )))
+    }
+
+    /// Create a notch filter.
+    pub fn notch(sample_rate: f64, frequency: f64, Q(q): Q) -> Self {
+        Self(juce::create_iir_filter(juce::make_notch_filter(
+            sample_rate,
+            frequency,
+            q,
+        )))
+    }
+
+    /// Filter the given samples.
+    pub fn process(&mut self, samples: &mut [f32]) {
+        unsafe {
+            self.0
+                .pin_mut()
+                .process_samples(samples.as_mut_ptr(), samples.len() as i32)
+        }
+    }
+}
