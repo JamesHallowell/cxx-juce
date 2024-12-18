7a603804ae80b1ec1760956afea471a0b1f2bf21
James Hallowell <5460955+JamesHallowell@users.noreply.github.com>
2024-12-18 22:34:09 +0000

    Simplify lifetimes with JUCE application handle
    See https://github.com/JamesHallowell/cxx-juce/issues/10

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,10 @@ use {
         },
         BoxedAudioIODevice, BoxedAudioIODeviceCallback, BoxedAudioIODeviceType,
     },
-    std::sync::{Mutex, MutexGuard},
+    std::{
+        rc::Rc,
+        sync::atomic::{AtomicBool, Ordering},
+    },
 };
 
 /// Returns the version of the JUCE library.
@@ -25,38 +28,74 @@ pub fn juce_version() -> String {
     juce::version()
 }
 
-/// An RAII guard for JUCE. Required for certain JUCE classes.
+/// A handle to the JUCE runtime. Required for certain JUCE classes.
+///
+/// Once all references to this object are dropped, the JUCE runtime will be shut down.
 #[must_use]
-pub struct JUCE<'juce> {
-    _guard: MutexGuard<'juce, ()>,
+#[derive(Clone)]
+pub struct JUCE {
+    _app: Rc<JuceApp>,
 }
 
-static JUCE_INSTANCE: Mutex<()> = Mutex::new(());
+static IS_JUCE_RUNNING: AtomicBool = AtomicBool::new(false);
 
-impl<'juce> JUCE<'juce> {
-    /// Initialise JUCE. Panics if JUCE is already initialised.
-    pub fn initialise() -> Self {
-        Self::new(JUCE_INSTANCE.try_lock().expect("JUCE already initialised"))
-    }
+struct JuceApp;
 
-    #[doc(hidden)]
-    pub fn wait_to_initialise_in_test_context() -> Self {
-        Self::new(JUCE_INSTANCE.lock().unwrap())
-    }
-
-    fn new(guard: MutexGuard<'juce, ()>) -> Self {
+impl JuceApp {
+    fn new() -> Self {
         juce::initialise_juce();
 
         #[cfg(target_os = "macos")]
         juce::initialise_ns_application();
 
-        Self { _guard: guard }
+        Self
     }
 }
 
-impl Drop for JUCE<'_> {
+impl Drop for JuceApp {
     fn drop(&mut self) {
         juce::shutdown_juce();
+
+        IS_JUCE_RUNNING.store(false, Ordering::SeqCst);
+    }
+}
+
+#[derive(Debug)]
+enum InitialiseError {
+    JuceAlreadyInitialised,
+}
+
+impl std::error::Error for InitialiseError {}
+
+impl std::fmt::Display for InitialiseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Self::JuceAlreadyInitialised => write!(f, "JUCE has already been initialised"),
+        }
+    }
+}
+
+impl JUCE {
+    /// Initialises the JUCE runtime.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the JUCE runtime is already initialised.
+    pub fn initialise() -> Self {
+        Self::try_initialise().unwrap()
+    }
+
+    fn try_initialise() -> std::result::Result<Self, InitialiseError> {
+        let result =
+            IS_JUCE_RUNNING.compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst);
+
+        if result.is_err() {
+            return Err(InitialiseError::JuceAlreadyInitialised);
+        }
+
+        Ok(Self {
+            _app: Rc::new(JuceApp::new()),
+        })
     }
 }
 
@@ -426,5 +465,61 @@ pub(crate) mod juce {
         #[namespace = "cxx_juce::iir_filter"]
         #[rust_name = "make_notch_filter"]
         pub fn makeNotchFilter(sample_rate: f64, frequency: f64, q: f64) -> [f32; 5];
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn try_to_initialise_juce_on_new_thread() -> std::thread::Result<()> {
+        std::thread::spawn(move || {
+            let _juce = JUCE::initialise();
+        })
+        .join()
+    }
+
+    #[test]
+    #[should_panic]
+    fn initialising_juce_twice_on_the_same_thread_should_panic() {
+        let _juce = JUCE::initialise();
+        let _juce = JUCE::initialise();
+    }
+
+    #[test]
+    fn initialising_juce_again_on_the_same_thread_after_shutdown_is_ok() {
+        let juce = JUCE::initialise();
+        drop(juce);
+
+        let _juce = JUCE::initialise();
+    }
+
+    #[test]
+    fn juce_cant_be_initialised_simultaneously_on_two_different_threads() {
+        let _juce = JUCE::initialise();
+
+        assert!(try_to_initialise_juce_on_new_thread().is_err());
+    }
+
+    #[test]
+    fn juce_can_run_on_a_different_thread_after_finishing_on_another() {
+        let juce = JUCE::initialise();
+        drop(juce);
+
+        assert!(try_to_initialise_juce_on_new_thread().is_ok());
+    }
+
+    #[test]
+    fn juce_is_shutdown_once_all_references_have_been_dropped() {
+        let a = JUCE::initialise();
+        let b = a.clone();
+
+        drop(a);
+
+        assert!(try_to_initialise_juce_on_new_thread().is_err());
+
+        drop(b);
+
+        assert!(try_to_initialise_juce_on_new_thread().is_ok());
     }
 }
